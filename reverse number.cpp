c07c4d2b65273451cdec5939e70b846c30f368a9
Zaid <shervanzeydhan@gmail.com>
2026-05-17 20:23:46 +0800

    reversing number

---
+++ reverse number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num, reversed = 0, remainder;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    while (num != 0) {
+    	remainder= num % 10;
+    	reversed = reversed *10 + remainder;
+    	num /= 10;
+    }
+    
+	cout<<"Reversed number: "<< reversed << endl;
+	return 0;
+}
